a6f21b66922cf7f818adb73e91e626fe29a375dd
volcanomakingkit <74361226+volcanomakingkit@users.noreply.github.com>
2022-01-03 09:46:01 +0100

    avoid to many tables

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+let maxTab = 4;
+let counter = 1
+
 $(document).ready(function(){
 	$('#boutton').on('click', function(){
-		$('.un').clone().appendTo($('#tout'));
-		document.getElementsByClassName('un')[0].className = "deux";
+		if(counter < 4) {
+			$('.un').clone().appendTo($('#tout'));
+		    document.getElementsByClassName('un')[0].className = "deux";
+		}
 	})
 });
